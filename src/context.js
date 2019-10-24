5d48301ce5c9a6ca878b883bea13503eb268aaa5
Sahil Thakur <thakursahil92@gmail.com>
2019-10-24 19:38:34 -0400

    paypal button added

--- src/context.js
+++ src/context.js
@@ -82,7 +82,7 @@ class ProductProvider extends Component {
 
     increment = (id) => {
         let tempCart = [...this.state.cart];
-        const selectedProduct = tempCart.find(item => item.id == id);
+        const selectedProduct = tempCart.find(item => item.id === id);
         
         const index = tempCart.indexOf(selectedProduct);
         const product = tempCart[index];
@@ -99,7 +99,7 @@ class ProductProvider extends Component {
 
     decrement = (id) => {
         let tempCart = [...this.state.cart];
-        const selectedProduct = tempCart.find(item => item.id == id);
+        const selectedProduct = tempCart.find(item => item.id === id);
         
         const index = tempCart.indexOf(selectedProduct);
         const product = tempCart[index];
